d5f3cafc1c0fcf688d78eebff8ae196752fbb52b
Jack Hamer <jackhamer0990@gmail.com>
2023-11-13 15:44:16 +0300

    fix: footer

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <footer class="container flex items-center gap-4 pb-4 pt-12 sm:gap-6">
+    <footer class="container flex flex-col items-center gap-4 pb-4 pt-12 sm:flex-row sm:justify-between sm:gap-6">
       <div class="text-left text-sm text-gray-400">
         Made with ❤️ by
         <a
@@ -16,8 +16,9 @@
         title="zkSync Plus GitHub page"
         target="_blank"
         rel="noopener"
-        class="block w-max text-gray-500 transition hover:text-gray-900"
+        class="flex w-max items-center gap-1.5 gap-x-1.5 whitespace-nowrap rounded-3xl bg-white/50 p-1 px-3 text-sm text-gray-500 shadow-md shadow-blue-800/5 ring-1 ring-gray-600/[.15] transition hover:bg-white hover:text-gray-900 hover:shadow-xl hover:shadow-blue-800/5 hover:ring-gray-300"
       >
+        <span>View source code</span>
         <IconsGitHub class="h-7 w-7" />
       </a>
     </footer>
